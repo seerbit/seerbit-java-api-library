b0b09e1f7a3c0ea53041a4287496306ff1a00ff6
centric123 <abubakar.eliakwu@yahoo.com>
2020-07-30 23:21:16 +0100

    updated

---
+++ src/main/java/com/seerbit/v2/demo/status/GetTransactionStatusDemo.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2020 Seerbit
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.seerbit.v2.demo.status;
+
+import com.google.gson.JsonObject;
+import com.seerbit.v2.Client;
+import com.seerbit.v2.Seerbit;
+import com.seerbit.v2.enums.EnvironmentEnum;
+import com.seerbit.v2.impl.SeerbitImpl;
+import com.seerbit.v2.service.AuthenticationService;
+import com.seerbit.v2.service.StatusService;
+import com.seerbit.v2.service.impl.AuthenticationServiceImpl;
+import com.seerbit.v2.service.impl.StatusServiceImpl;
+
+/**
+ * @author Seerbit
+ */
+public class GetTransactionStatusDemo {
+
+	private static Client client;
+
+	/**
+	 * @return token (java.lang.String)
+	 */
+	private static String doAuthenticate() {
+		Seerbit seerbit;
+		String token;
+		AuthenticationService authService;
+		JsonObject json;
+		String jsonString;
+
+		System.out.println("================== start authentication ==================");
+		seerbit = new SeerbitImpl();
+		client = new Client();
+		client.setApiBase(seerbit.getApiBase());
+		client.setEnvironment(EnvironmentEnum.LIVE.getEnvironment());
+		client.setPublicKey("public_key");
+		client.setPrivateKey("private_key");
+		client.setTimeout(20);
+		authService = new AuthenticationServiceImpl(client);
+		json = authService.doAuth();
+		jsonString = String.format("auth response: \n%s", json.toString());
+		System.out.println(jsonString);
+		System.out.println("================== end authentication ==================");
+		System.out.println("\n");
+		System.out.println("\n");
+		token = authService.getToken();
+
+		return token;
+	}
+
+	/**
+	 * @param token     (java.lang.String)
+	 * @param reference (java.lang.String)
+	 *
+	 * @return response
+	 */
+	private static JsonObject doQueryStatus(String token, String reference) {
+		StatusService statusService;
+		JsonObject response;
+
+		System.out.println("================== start get transaction status ==================");
+		statusService = new StatusServiceImpl(client, token);
+		response = statusService.getTransactionStatus(reference);
+		System.out.println("================== stop get transaction status ==================");
+
+		return response;
+	}
+
+	/**
+	 * @param args String arguments array for main function
+	 */
+	public static void main(String... args) {
+		String token;
+		String reference;
+		JsonObject response;
+
+		reference = "227171662717";
+		token = GetTransactionStatusDemo.doAuthenticate();
+		response = GetTransactionStatusDemo.doQueryStatus(token, reference);
+		System.out.println("get transaction status response: " + response.toString());
+	}
+}
