502e1f8e20d0b7a3f25985e29b5fcb85ec19c5d6
centric123 <abubakar.eliakwu@yahoo.com>
2020-07-31 00:14:23 +0100

    updated

---
+++ src/main/java/com/seerbit/v2/demo/recurrent/UpdateRecurrentDemo.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2020 Seerbit
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.seerbit.v2.demo.recurrent;
+
+import com.google.gson.JsonObject;
+import com.seerbit.v2.Client;
+import com.seerbit.v2.Seerbit;
+import com.seerbit.v2.enums.EnvironmentEnum;
+import com.seerbit.v2.impl.SeerbitImpl;
+import com.seerbit.v2.model.Subscription;
+import com.seerbit.v2.service.AuthenticationService;
+import com.seerbit.v2.service.RecurringService;
+import com.seerbit.v2.service.impl.AuthenticationServiceImpl;
+import com.seerbit.v2.service.impl.RecurringServiceImpl;
+
+/**
+ * @author Seerbit
+ */
+public class UpdateRecurrentDemo {
+
+	private static Client client;
+
+	/**
+	 * @return token (java.lang.String)
+	 */
+	private static String doAuthenticate() {
+		Seerbit seerbit;
+		String token;
+		AuthenticationService authService;
+		JsonObject json;
+		String jsonString;
+
+		System.out.println("================== start authentication ==================");
+		seerbit = new SeerbitImpl();
+		client = new Client();
+		client.setApiBase(seerbit.getApiBase());
+		client.setEnvironment(EnvironmentEnum.LIVE.getEnvironment());
+		client.setPublicKey("public_key");
+		client.setPrivateKey("private_key");
+		client.setTimeout(20);
+		authService = new AuthenticationServiceImpl(client);
+		json = authService.doAuth();
+		jsonString = String.format("auth response: \n%s", json.toString());
+		System.out.println(jsonString);
+		System.out.println("================== end authentication ==================");
+		System.out.println("\n");
+		System.out.println("\n");
+		token = authService.getToken();
+
+		return token;
+	}
+
+	/**
+	 * @param token (java.lang.String)
+	 *
+	 * @return response
+	 */
+	private static JsonObject doUpdateSubscription(String token) {
+		RecurringService recurringService;
+		Subscription subscription;
+		JsonObject response;
+
+		System.out.println("================== start update subscription ==================");
+		subscription = Subscription
+			.builder()
+			.amount("20")
+			.country("KE")
+			.currency("KES")
+			.mobileNumber("08030540611")
+			.billingId("PUBK_PjQ5d1578650322483")
+			.publicKey(client.getPublicKey())
+			.status("INACTIVE")
+			.build();
+		recurringService = new RecurringServiceImpl(client, token);
+		response = recurringService.doUpdateSubscription(subscription);
+		System.out.println("================== end update subscription ==================");
+
+		return response;
+	}
+
+	/**
+	 * @param args String arguments array for main function
+	 */
+	public static void main(String... args) {
+		String token;
+		JsonObject response;
+
+		token = UpdateRecurrentDemo.doAuthenticate();
+		response = UpdateRecurrentDemo.doUpdateSubscription(token);
+		System.out.println("get update subscription response: " + response.toString());
+	}
+}
